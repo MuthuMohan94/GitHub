cdc24e05356a3555a626b78eb67807e6f320c7b4
Muthu Mohan <muthumohan010@gmail.com>
2018-02-04 23:53:23 -0800

    Initialization of Repository

---
+++ SpaceInvaders/src/spaceInvaders/CommonInterface.java
@@ -0,0 +1,7 @@
+package spaceInvaders;
+
+public interface CommonInterface {
+	static final int BOARD_WIDTH = 150;
+	static final int BOARD_LENGTH = 150;
+
+}
---
+++ SpaceInvaders/src/spaceInvaders/SpaceInvaders.java
@@ -0,0 +1,15 @@
+package spaceInvaders;
+
+public class SpaceInvaders implements CommonInterface {
+	
+	SpaceInvaders() {
+		// Call Board
+		// CAll player
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
