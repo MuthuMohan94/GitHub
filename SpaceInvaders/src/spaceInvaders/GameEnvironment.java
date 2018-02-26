e37c3085dd674a567e2d573cdf70dee37be308e5
shubhrabhangui <shubhy30@gmail.com>
2018-02-25 19:45:34 -0800

    Update GameEnvironment.java
    Basic structure as shown in class diagram

--- SpaceInvaders/src/spaceInvaders/GameEnvironment.java
+++ SpaceInvaders/src/spaceInvaders/GameEnvironment.java
@@ -1,7 +1,66 @@
-package spaceInvaders;
+package space_Invaders;
+
 
 import javax.swing.JPanel;
 
 public class GameEnvironment extends JPanel implements CommonInterface {
+	// set dimensions
+	protected final static int noOfDeaths=3;
+	protected final static int speed=5;
+	/*
+	 * protected final static int movementDirection;
+	 * protected final static int speed;
+	 * dimensions 
+	 */
+	
+	GameEnvironment()
+	{
+		initializeBoard();
+	}
+	
+	void initializeBoard()
+	{
+		
+	}
+	
+	void drawAliens()
+	{
+		
+	}
+	
+	void drawPlayer()
+	{
+		
+	}
+	
+	void drawShot()
+	{
+		
+	}
+	
+	void drawEnemnyShot()
+	{
+		
+	}
+	
+	void drawComponent()
+	{
+	
+	}
+	
+	void endOfGame()
+	{
+	
+	}
+	
+	void setSpeed(int speed)
+	{
+	
+	}
+	
+	int getSpeed()
+	{
+	    return 0;
+	}
 
 }
