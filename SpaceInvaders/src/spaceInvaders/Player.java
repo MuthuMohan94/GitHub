b3f15d2a7a207349523b7a666481709acb93e8ad
shubhrabhangui <shubhy30@gmail.com>
2018-02-25 23:18:37 -0800

    player.java

--- SpaceInvaders/src/spaceInvaders/Player.java
+++ SpaceInvaders/src/spaceInvaders/Player.java
@@ -1,6 +1,6 @@
 package space_Invaders;
 
-public class Player extends GameEnvironment {
+public class Player extends Model implements CommonInterface  {
 	
 	protected String playerIconLocation;
 	protected int startXPos;
