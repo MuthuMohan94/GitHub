68007d2bcaea20fcf18e415882f4cdc61ba7086f
shubhrabhangui <shubhy30@gmail.com>
2018-02-26 00:42:29 -0800

    GameEnvironment.java

--- SpaceInvaders/src/spaceInvaders/GameEnvironment.java
+++ SpaceInvaders/src/spaceInvaders/GameEnvironment.java
@@ -3,6 +3,7 @@ package space_Invaders;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,6 +13,8 @@ public class GameEnvironment extends JPanel implements CommonInterface{
 	protected final static int noOfDeaths=3;
 	protected final static int speed=5;
 	protected Dimension board;
+	
+	protected Player player;
 	/*
 	 * protected final static int movementDirection;
 	 * protected final static int speed;
@@ -26,7 +29,8 @@ public class GameEnvironment extends JPanel implements CommonInterface{
 	void initializeBoard()
 	{
 		board = new Dimension(boardWidth, boardLength);
-                setBackground(Color.black);
+        setBackground(Color.black);
+        player= new Player();
 	}
 	
 	void drawAliens()
@@ -34,8 +38,18 @@ public class GameEnvironment extends JPanel implements CommonInterface{
 		
 	}
 	
-	void drawPlayer()
+	void drawPlayer(Graphics draw_graphics)
 	{
+	{   
+		//Player.paintIcon(this, draw_graphics, player.getXPos(), player.getYPos());
+		draw_graphics.drawImage(player.getIcon(), player.getXPos(), player.getYPos(), this);
+    }
+
+    /*if (player.getIsDead()) {
+
+        player.die();
+        ingame = false;
+    }*/
 		
 	}
 	
