be431d5ad520198d99bca72f516caa11803c7727
shubhrabhangui <shubhy30@gmail.com>
2018-02-25 20:11:18 -0800

    Create Player.java

---
+++ SpaceInvaders/src/spaceInvaders/Player.java
@@ -0,0 +1,14 @@
+package space_Invaders;
+
+public class Player extends GameEnvironment {
+	
+	protected String playerIconLocation;
+	protected int startXPos;
+	protected int startYPos;
+	// Constructor
+	Player()
+	{
+		
+	}
+
+}
