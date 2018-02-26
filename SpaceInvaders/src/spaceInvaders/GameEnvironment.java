6a34256cb8312024568520ced8867cc5e43a3a1b
shubhrabhangui <shubhy30@gmail.com>
2018-02-25 22:06:49 -0800

    Update GameEnvironment.java
    This one initializes the board

--- SpaceInvaders/src/spaceInvaders/GameEnvironment.java
+++ SpaceInvaders/src/spaceInvaders/GameEnvironment.java
@@ -1,12 +1,17 @@
 package space_Invaders;
 
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GameEnvironment extends JPanel implements CommonInterface {
+public class GameEnvironment extends JPanel implements CommonInterface{
 	// set dimensions
 	protected final static int noOfDeaths=3;
 	protected final static int speed=5;
+	protected Dimension board;
 	/*
 	 * protected final static int movementDirection;
 	 * protected final static int speed;
@@ -20,7 +25,8 @@ public class GameEnvironment extends JPanel implements CommonInterface {
 	
 	void initializeBoard()
 	{
-		
+		board = new Dimension(boardWidth, boardLength);
+        setBackground(Color.black);
 	}
 	
 	void drawAliens()
