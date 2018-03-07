31181fed70cbe029382a61fd3f585355ba3b920b
AnushaSuresh12 <35473397+AnushaSuresh12@users.noreply.github.com>
2018-03-06 16:43:38 -0800

    Add files via upload

---
+++ SpaceInvaders/src/spaceInvaders/Bomb.java
@@ -0,0 +1,27 @@
+package invaders;
+
+import javax.swing.ImageIcon;
+
+public class Bomb extends Model {
+
+    private final String shotImg = "src/images/shot.png";
+    private final int H_space = 6;
+    private final int V_space = 1;
+
+    public Bomb() {
+    }
+
+    public Bomb(int x, int y) {
+
+        initShot(x, y);
+    }
+
+    private void initShot(int x, int y) {
+
+        ImageIcon ii = new ImageIcon(shotImg);
+        setImg(ii.getImage());
+        
+        setX(x + H_space);
+        setY(y - V_space);
+    }
+}
