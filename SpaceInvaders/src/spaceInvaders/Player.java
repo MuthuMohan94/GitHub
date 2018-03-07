3e1b849e5259ae0c06f581ac8cc5c54305d8a523
AnushaSuresh12 <35473397+AnushaSuresh12@users.noreply.github.com>
2018-03-06 16:36:25 -0800

    I have included left and right cursor movement

--- SpaceInvaders/src/spaceInvaders/Player.java
+++ SpaceInvaders/src/spaceInvaders/Player.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+
 import javax.swing.ImageIcon;
 
 public class Player extends Model implements CommonInterface  {
@@ -6,6 +8,7 @@ public class Player extends Model implements CommonInterface  {
     private int startYPos = 340;
 
     private final String playerImg = "src/img/ship.png";
+    private int Width;
 
 
     public Player() {
@@ -13,11 +16,51 @@ public class Player extends Model implements CommonInterface  {
     }
 	
 	void initPlayer(){		
-		ImageIcon playerIcon= new ImageIcon(playerImg);
-		setImg(playerIcon.getImage());
+		ImageIcon i= new ImageIcon(playerImg);
+		Width = i.getImage().getWidth(null);
+		setImg(i.getImage());
 		setX(startXPos);
 		setY(startYPos);		
 	}
 	
+	public void act()
+	{
+		
+		x+=d;
+		if(x<=2) {
+			x=2;
+		}
+		if(x>=BOARD_WIDTH-2*Width)
+		{
+			x=BOARD_WIDTH-2*Width;
+		}
+	}
+	public void keypressed(KeyEvent e)
+	{
+		int k=e.getKeyCode();
+		// if left cursor key pressed
+		if(k==KeyEvent.VK_LEFT)
+		{
+			d=-2;
+		}
+		//if right cursor key pressed
+		if(k==KeyEvent.VK_RIGHT)
+		{
+			d=2;
+		}
+	}
+	// if both keys are released
+	public void keyreleased(KeyEvent e)
+	{
+		int k=e.getKeyCode();
+		if(k==KeyEvent.VK_LEFT)
+		{
+			d=0;
+		}
+	if(k==KeyEvent.VK_RIGHT)
+	{
+		d=0;
+	}
+	}
 
 }
