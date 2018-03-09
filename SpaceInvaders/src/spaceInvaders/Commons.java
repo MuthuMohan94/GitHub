0aca0579f69a9592e2042b1dfe62d37ee9060c32
Muthu Mohan <muthumohan010@gmail.com>
2018-03-08 22:57:57 -0800

    changed interface

---
+++ SpaceInvaders/src/spaceInvaders/Commons.java
@@ -0,0 +1,18 @@
+package spaceInvaders;
+
+public interface Commons {
+    public static final int BOARD_WIDTH = 600;
+    public static final int BOARD_HEIGHT = 600;
+    public static final int GROUND = 550;
+    public static final int BOMB_HEIGHT = 5;
+    public static final int ALIEN_HEIGHT = 12;
+    public static final int ALIEN_WIDTH = 12;
+    public static final int BORDER_RIGHT = 30;
+    public static final int BORDER_LEFT = 5;
+    public static final int GO_DOWN = 15;
+    public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
+    public static final int CHANCE = 5;
+    //public static final int DELAY = 5;
+    public static final int PLAYER_WIDTH = 15;
+    public static final int PLAYER_HEIGHT = 10;
+}
